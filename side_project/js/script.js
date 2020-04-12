f84cba6e1426e89e073e99f421e246345db31a94
osaelh <oussamaelh@gmail.com>
2020-04-12 21:19:17 +0100

    side_project js_clock

---
+++ side_project/js/script.js
@@ -0,0 +1,34 @@
+
+const SCORPHR = document.querySelector("#hour");
+const SCORPMIN = document.querySelector("#minute");
+const SCORPSEC = document.querySelector("#second");
+
+
+
+ 
+function demarrerLaMontre() {
+
+    var  myDate = new Date ();
+    let myHour = myDate.getHours() ;
+    let myMinute = myDate.getMinutes();
+    let mySecond= myDate.getSeconds();
+
+    //  console.log( "Hour:  "+ myHour+ " Minute: "+  myMinute+ " Second:"+ mySecond);
+
+    let hr = myHour*360/12 + ((myMinute * 360/60)/12) ;
+    let min = (myMinute * 360/60) + (mySecond* 360/60)/60;
+    let sec = mySecond * 360/60;
+
+
+    SCORPHR.style.transform = `rotate(${hr}deg)`;
+    SCORPMIN.style.transform = `rotate(${min}deg)`;
+    SCORPSEC.style.transform = `rotate(${sec}deg)`;
+
+
+
+
+}
+
+setInterval(demarrerLaMontre, 1000);
+
+demarrerLaMontre();
