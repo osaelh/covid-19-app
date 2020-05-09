19e59ce71369bf30e1d63ff359eabb68a515ad69
osaelh <oussamaelh@gmail.com>
2020-05-09 18:59:24 +0000

    test 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,96 +1,240 @@
-const questions = [
-  "Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
-  "Quelle est votre température corporelle ?",
-  "Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
-  "Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
-  "Ces derniers jours, avez-vous un mal de gorge ?",
-  "Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles.",
-  "Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?",
-  "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?",
-  "Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?",
-  "Actuellement, comment vous vous sentez ?",
-  "Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.",
-  "Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
-  "Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
-  "Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?",
-  "Êtes-vous diabétique ?",
-  "Avez-vous ou avez-vous eu un cancer ?",
-  "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?",
-  "Avez-vous une insuffisance rénale chronique dialysée ?",
-  "Avez-vous une maladie chronique du foie ?",
-  "Êtes-vous enceinte ?",
-  "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?",
-  "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."  
+let questions = [{
+  question: "Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
+
+  input: {
+      type: "radio",
+      qNumber: "Q1",
+      answer: [{
+              text: "Oui",
+              icon: "fa-check",
+          },
+          {
+              text: "Non",
+              icon: "fa-times",
+          },
+      ],
+  },
+},
+{
+  question: "Quelle est votre température corporelle ?",
+
+  input: {
+      type: "number",
+      qNumber: "Q2",
+      name: "degrés",
+      min: 34,
+      max: 42,
+  },
+},
+{
+  question: "Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
+
+  input: {
+      type: "radio",
+      qNumber: "Q3",
+      answer: [{
+              text: "Oui",
+              icon: "fa-check",
+          },
+          {
+              text: "Non",
+              icon: "fa-times",
+          },
+      ],
+  },
+},
+{
+  question: "Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
+
+  input: {
+      type: "radio",
+      qNumber: "Q4",
+      answer: [{
+              text: "Oui",
+              icon: "fa-check",
+          },
+          {
+              text: "Non",
+              icon: "fa-times",
+          },
+      ],
+  },
+},
 ];
-const startBtn = document.querySelector('#start');
-const wrapper = document.querySelector('#main');
-const precedent = document.querySelector('#precedent');
-const suivant = document.querySelector('#suivant');
-const resultat = document.querySelector('#resultat');
-const form  = document.querySelector('#form');
-const questionnaireBullet = document.querySelector('#bullet2');
-const question = document.querySelector('.questions')
-console.log(precedent)
-console.log(questions)
+let startBtn = document.getElementById("start");
+let questionnaire = document.getElementById("questionnaire");
+let Préambule = document.getElementById("Préambule");
+let stepper = document.querySelectorAll(".stepper h1");
+let nextBtn = document.querySelector(".next");
+let previousBtn = document.querySelector(".previous");
+let currentquestion = document.querySelector(".question");
+let answerInputs = document.querySelector(".answer-inputs");
+let progressBar = document.querySelector(".bar");
+let questionNumber = document.querySelector(".question-number");
+let inputBox = document.querySelector(".inputBox");
 
-var conter = 0;
-var reponces = [];
-var val = 1;
+let result = document.querySelector(".header");
+let resultMessage = document.querySelector(".end"); 
+let resultMessage2 = document.querySelector('.end2')  
 
-function testStart(){
-  wrapper.style.display = 'none';
-  precedent.style.display = 'none';
-  resultat.style.display = 'none';
-  form.style.display = 'block';
-  startBtn.style.display = 'none';
-  questionnaireBullet.className ='bullet completed';
-  question.innerHTML = questions[0];
-  nextQuestion();
-  
+console.log(previousBtn)
+let currentQuestionIndex = 0;
+
+
+startBtn.addEventListener("click", startTest);
+function startTest() {
+  stepper[0].classList.remove("active");
+  stepper[1].classList.add("active");
+  startBtn.style.display = "none";
+  Préambule.style.display = "none";
+  questionnaire.style.display = "block";
+  previousBtn.style.display ='none'
+  startBtn.disabled = true;
+  showQuestion(questions[currentQuestionIndex]);
 }
 
-startBtn.addEventListener('click',testStart);
-suivant.addEventListener('click',nextQuestion);
+function showQuestion(question) {
+  currentquestion.innerText = question.question;
+  answerInputs.innerHTML = "";
+  let inputAnswer = question.input.answer;
+  let input = question.input;
 
-function nextQuestion(e){
-  if(conter >= 0 && conter < questions.length -1){
-    conter++;
+  if (question.input.type === "radio") {
+      inputAnswer.forEach((answer) => {
+          answerInputs.innerHTML += `
+                  <div>
+                      <input type="radio" name="${input.qNumber}" id="${answer.text}">
+                      <label for="${answer.text}">
+                      <i class="fas ${answer.icon}"></i>
+                      <span>${answer.text}</span> </label>
+                  </div>`;
+      });
+  } else {
+      answerInputs.innerHTML += `<input type="number" name="${input.qNumber}" id="${input.name}" min="${input.min}" max="${input.max}" placeholder="${input.min} - ${input.max}">
+                                  <span class="input-span">${input.name}</span>`;
   }
-  // if(conter == questions.length){
-  //   suivant.style.display ='none';
-  //   resultat.style.display ='block';
-  // }
-  // if(conter == questions.length - 21){
-  //   suivant.style.display ='block';
-  //   resultat.style.display ='none';
-  //   precedent.style.display = 'none'
-  // }
-  precedent.style.display ='block';
-  question.innerHTML=questions[conter];
-  e.preventDefault();
 }
-// let progress = 1
-// showSlide(progress);
+
+nextBtn.addEventListener('click', nextQuestion)
+
+function nextQuestion() {
+  if (currentQuestionIndex < 3) {
+    currentQuestionIndex++;
+    showQuestion(questions[currentQuestionIndex]);
+    // folowProgress(currentQuestionIndex);
+    
+    previousBtn.style.display ='block'
+
+    nextBtn.disabled = false;
+  }
+    if (currentQuestionIndex === 3) {
+        nextBtn.innerText = "Terminer le test";
+        nextBtn.classList.add("result");
+        let resultBtn = document.querySelector(".result");
+        resultBtn.addEventListener("click", Results);
+    } else {
+        nextBtn.innerText = "Suivant";
+        previousBtn.style.display ='block'
+        
+    }
+  }
+
+  previousBtn.addEventListener('click',previousQuestion);
+
+  function previousQuestion() {
+    if (currentQuestionIndex < 3){
+    currentQuestionIndex--;
+    showQuestion(questions[currentQuestionIndex]);
+    // folowProgress(currentQuestionIndex);
+    nextBtn.disabled = true;}
+    if (currentQuestionIndex === 3) {
+        nextBtn.innerText = "Terminer le test";
+    } else {
+        nextBtn.innerText = "Suivant";
+        nextBtn.classList.remove("result");
+    }
+  }
+  inputBox.addEventListener("change", (event) => {
+    let input = event.target;
+
+    if (input.type === "number") {
+        let number = parseFloat(input.value);
+
+        if (number >= input.min && number <= input.max) {
+            answers[input.name] = input.value;
+            console.log(answers);
+
+            nextBtn.disabled = false;
+        } else {
+            nextBtn.disabled = true;
+        }
+    } else {
+        answers[input.name] = input.id;
+        console.log(answers);
+        nextBtn.disabled = false;
+    }
+});
+
+let answers = {};
+let severity = 0;
+
+function Results() {
 
 
-// function nextQuestion( n ){
-//   showSlide( progress += n );
-// }
-
-// function showSlide(n){
-//   if (n === 1){
-//     precedent.style.display = 'none';
-//   }else{
-//    precedent.style.display = 'block';
-//   }
-
-//   if ( n === questions.length - 1){
-//     suivant.style.display = 'none';
-//   }else{
-//     suivant.style.display ='block';
-//   }
-
+  //patient with fever or a  cough
   
+      if (((answers['Q1'] === 'Oui' || answers['Q3'] === 'Oui') || (answers['Q1'] === 'Oui' || answers['Q4'] === 'Oui') )) {
+  
+          resultMessage.innerText = 'Nous vous conseillons de rester à votre domicile et de contacter votre médecin' +
+                  ' en cas d’apparition de nouveaux symptômes. Vous pourrez aussi utiliser à nouv' +
+                  'eau l’application pour réévaluer vos symptômes'
+          resultMessage2.innerText = 'Restez chez vous au maximum en attendant que les symptômes disparaissent. Pren' +
+                  'ez votre température deux fois par jour. Rappel des mesures d’hygiène.'
+          resultMessage.style.fontWeight = 'bold'
+          resultMessage.style.color = '#369D53'
+      }
+// patient with one symptome amongst fever, cough and body pain
+      if (((answers['Q1'] === 'Oui' && answers['Q3'] === 'Non' && answers['Q4'] === 'Non') || (answers['Q1'] === 'Non' && answers['Q3'] === 'Oui' && answers['Q4'] === 'Non') || (answers['Q4'] === 'Oui' && answers['Q1'] == 'Non' && answers['Q3'] == 'Non'))) {
+  
+        resultMessage.innerText = 'Votre situation ne relève probablement pas du Covid-19. Consultez votre médecin au moindre doute.'
+        resultMessage2.innerText = 'Restez chez vous au maximum en attendant que les symptômes disparaissent. Pren' +
+                'ez votre température deux fois par jour. Rappel des mesures d’hygiène.'
+        resultMessage.style.fontWeight = 'bold'
+        resultMessage.style.color = '#369D53'
+    }
 
-//   question.innerHTML = questions[n];
-// }
+    //patient with fever and cough 
+    if (((answers['Q1'] === 'Oui' && answers['Q3'] === 'Oui' && answers['Q4'] == 'Non'))) {
+      resultMessage.innerText = 'Nous vous conseillons de rester à votre domicile et de contacter votre médecin' +
+                  ' en cas d’apparition de nouveaux symptômes. Vous pourrez aussi utiliser à nouv' +
+                  'eau l’application pour réévaluer vos symptômes'
+          resultMessage.style.fontWeight = 'bold'
+          resultMessage.style.color = '#369D53'
+    }
+  
+  
+  
+  
+     showResult(severity)
+  
+  }
+function showResult(severity) {
+
+  stepper[1].classList.remove('active')
+      
+      
+  stepper[2] .classList.add('active')
+     
+      
+  startBtn.style.display = 'block'
+  Préambule.style.display = 'block'
+  questionnaire.style.display = 'none'
+  startBtn.textContent = ' Recommencer le test'
+  startBtn.addEventListener('click', reload)
+
+  header.innerText = "Résultats";
+}
+
+function reload() {
+  window.location.reload()
+}
